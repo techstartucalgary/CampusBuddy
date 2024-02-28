9226bfef0ac2cd69aa91c73c3b7e5bfbe6863ad6
JamesRobert20 <jmtendamema@gmail.com>
2024-02-28 10:16:43 -0700

    context and react query examples
    Signed-off-by: JamesRobert20 <jmtendamema@gmail.com>

--- frontend/src/contexts/eventsContext.tsx
+++ frontend/src/contexts/eventsContext.tsx
@@ -1,4 +1,5 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import type { PropsWithChildren } from "react";
 import { createContext } from "react";
 import { mockSearchEvents } from "~/mockData/EventData";
@@ -13,19 +14,46 @@ type Event = {
 }
 
 type eventsContext = {
-    events: Event[]
+    events: Event[],
+    eventsByUOfC: Event[]
 };
 const EventsContext = createContext<eventsContext | null>(null);
 
 export const EventsContextProvider = ({ children }: PropsWithChildren): JSX.Element => {
-    const { data: events } = useQuery({
+    const { data: events, refetch: refreshEvents } = useQuery({
         queryKey: ['events'],
-        queryFn: async () => mockSearchEvents,
+        queryFn: async () => {
+            let response = await (await fetch('/api/getAllEvents')).json();
+            let response2 = await axios.get('/api/getAllEvents');
+            return mockSearchEvents
+        },
         initialData: []
     })
 
+    const { variables, mutate, isPending } = useMutation({
+        mutationFn: async ({ type }: any) => {
+            if(type === 'likingEvent') {
+                // code to like an event
+            }
+            else if(type === 'createEvent') {
+                // code to create an event
+            }
+            refreshEvents()
+        }
+    });
+
+    const likeEvent = () => {
+        mutate({ type: 'likingEvent'})
+    }
+
+    const createEvent = () => {
+        mutate({ type: 'createEvent'})
+    }
+
+    const eventsByUOfC =  events.filter(event => event.clubName === "uofc")
+
     return (
-        <EventsContext.Provider value={{ events }}>
+        <EventsContext.Provider value={{ events, eventsByUOfC }}>
             {children}
         </EventsContext.Provider>
     );
--- frontend/src/screens/ProfileEvents.tsx
+++ frontend/src/screens/ProfileEvents.tsx
@@ -1,7 +1,10 @@
 import { View } from "react-native";
 import { ThemedText } from "~/components/ThemedComponents";
+import useEventsContext from "~/hooks/useEventsContext";
 
 export default function ProfileEvents() {
+    const { eventsByUOfC } = useEventsContext();
+
     return (
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
             <ThemedText>Your Events</ThemedText>
