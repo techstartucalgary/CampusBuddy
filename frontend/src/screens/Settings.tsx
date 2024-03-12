0afa6910e8456149b2434058054d411ff29aaed4
Md-Rafi_zaman <rafi.shakik19@gmail.com>
2024-03-12 17:11:15 -0600

    Settings and privacy
    Signed-off-by: Md-Rafi_zaman <rafi.shakik19@gmail.com>

--- frontend/src/screens/Settings.tsx
+++ frontend/src/screens/Settings.tsx
@@ -1,36 +1,91 @@
+import {
+  BottomSheetBackdrop,
+  BottomSheetBackdropProps,
+  BottomSheetModal,
+} from "@gorhom/bottom-sheet";
+import { useCallback } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableHighlight,
+} from "react-native";
+import useProfileContext from "~/hooks/useProfileContext";
+import { Ionicons } from "@expo/vector-icons";
+import useThemeContext from "~/hooks/useThemeContext";
+import { useNavigation } from "@react-navigation/native";
 
-import React from "react";
-import { View, Modal, Text, TouchableOpacity } from "react-native";
+const snapPoints = ["45%"];
 
-export default function Settings() {
-  const [showPopup, setShowPopup] = React.useState(false);
+const settings = [
+  "Notifications",
+  "Search History",
+  "Blocked Users",
+  "Payments",
+  "Requests",
+];
+
+export default function ProfileSettings() {
+    const { theme } = useThemeContext();
+    const { bottomSheetModalRef } = useProfileContext();
+    const Backdrop = useCallback(
+    (props: BottomSheetBackdropProps) => (
+        <BottomSheetBackdrop {...props} disappearsOnIndex={-1} />
+    ),
+    [],
+    );
+    const { closeModal } = useProfileContext();
+    const navigation = useNavigation<any>();
+
+    const goToSettingsPage = useCallback((setting: string) => {
+        closeModal()
+        navigation.navigate("Settings", { setting })
+    }, [])
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <TouchableOpacity onPress={() => setShowPopup(true)}>
-        <Text style={{ fontSize: 20 }}>Open Popup</Text>
-      </TouchableOpacity>
-
-      {/* Pop-up */}
-      <Modal
-        visible={showPopup}
-        transparent
-        animationType="slide"
-      >
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
-          <View style={{ backgroundColor: 'white', padding: 20, borderRadius: 10 }}>
-            <Text style={{ fontSize: 18, marginBottom: 10 }}>Settings and Privacy</Text>
-            <Text>Notifications</Text>
-            <Text>Search History</Text>
-            <Text>Blocked Users</Text>
-            <Text>Payments</Text>
-            <Text>Requests</Text>
-            <TouchableOpacity onPress={() => setShowPopup(false)}>
-              <Text style={{ color: 'blue', marginTop: 20 }}>Close</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </Modal>
-    </View>
+    <BottomSheetModal
+      enablePanDownToClose={true}
+      ref={bottomSheetModalRef}
+      index={0}
+      snapPoints={snapPoints}
+      backdropComponent={Backdrop}
+      backgroundStyle={{ borderRadius: 20 }} // play around with the number to find a suitable one
+    >
+      <View style={styles.contentContainer}>
+        {settings.map((setting, i) => (
+          <TouchableOpacity key={i} onPress={() => goToSettingsPage(setting)}>
+            <View
+              style={[
+                styles.settingContainer,
+                {
+                  borderBottomColor: theme.colors.backdrop,
+                },
+              ]}
+            >
+              <Text style={styles.settingText}>{setting}</Text>
+              <Ionicons name="chevron-forward" size={16} color={"#3a86ff"} />
+            </View>
+          </TouchableOpacity>
+        ))}
+      </View>
+    </BottomSheetModal>
   );
 }
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    flex: 1,
+    padding: 20,
+  },
+  settingContainer: {
+    paddingVertical: 14,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomWidth: 0.8,
+  },
+  settingText: {
+    fontSize: 16,
+  },
+});
